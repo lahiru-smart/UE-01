e58196e7bc19b38d598795a34e1fff274b37095f
lahiru-smart <lahirusmart86@gmail.com>
2026-06-06 17:05:10 +0530

    ui-fix

---
+++ Sass/sass-app/pages/index.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="container">
+    <h1>Starter Page</h1>
+    <p>This is a starter page for your Nuxt application.</p>
+
+    <div class="card">
+      <div class="card__title">This is a card title</div>
+      <div class="card__body">
+        This is the body of the card. You can add any content here.
+        <a href="#">Learn more</a>
+      </div>
+    </div>
+  </div>
+</template>
