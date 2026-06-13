164afbde242e9c802378b0cf874a8b1a936e3911
lahiru-smart <lahirusmart86@gmail.com>
2026-06-13 16:38:57 +0530

    ui-add-sass

---
+++ Sass/sass-app/pages/challenges/01-variables/index.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="card">
+    <h2 class="card__title">This is a card</h2>
+    <p class="card__text">
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+      tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+      veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+      commodo consequat.
+    </p>
+    <div class="flex-group">
+      <a href="#" class="btn btn--primary">Buy now</a>
+      <a href="#" class="btn btn--outline">More info</a>
+    </div>
+  </div>
+</template>
+
+<style scoped lang="scss">
+@use "~/assets/sass/variable-challenge.scss";
+</style>
---
+++ Sass/sass-app/pages/modules/01-variables/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="container">
+    <h2 class="title">Variables</h2>
+    <p>Let's learn how to use variables in SCSS</p>
+    <button class="btn">Buy now</button>
+    <div class="card">
+      <h3 class="card__title">Card title</h3>
+      <a class="card-nav-link">Learn more</a>
+    </div>
+  </div>
+</template>
+
+<style scoped lang="scss">
+@use "~/assets/sass/variables.scss";
+</style>
